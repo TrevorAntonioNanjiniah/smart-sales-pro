484d0cd867d04ffe51413bcf4f3e843ef34af092
TrevorAntonioNanjiniah <nanjiniahtrevor@gmail.com>
2026-03-09 22:48:35 +0300

    Resolve merge conflicts and remove Next.js build files

--- frontend/.next/dev/static/chunks/92d79_@clerk_nextjs_dist_esm_app-router_keyless-actions_cfbc91db.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/92d79_@clerk_nextjs_dist_esm_app-router_d1142cd5._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/Desktop_smart-sales-assistant_frontend_app_favicon_ico_mjs_2bcb8b57._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/92d79_next_dist_ec8da92b._.js"
-],
-    source: "dynamic"
-});
